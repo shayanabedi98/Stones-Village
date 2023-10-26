f293b6ada02409f35760a5ab7e42fdb9b2fa6111
shayanabedi98 <shayanabedi@hotmail.com>
2023-10-26 19:49:23 -0400

    make homepage responsive for Mobile L

---
+++ .history/index_20231026172552.js
@@ -0,0 +1,92 @@
+const banner = document.querySelector('.banner1');
+const bannerImg = ['./resources/bg1', './resources/bg2', './resources/bg3', './resources/bg4'];
+let bannerImgIndex = 0;
+const logoName = document.querySelector('#logo-name');
+const logoCube = document.querySelector('#logo-cube');
+const navbar = document.querySelector('.navbar');
+const instagram = document.querySelector('#example-instagram');
+const instagramBanner = document.querySelector('.banner-overlay button');
+
+if (instagramBanner) {
+    instagramBanner.addEventListener('click', () => {
+        window.location.href = './inventory';
+    });
+}
+
+
+logoCube.addEventListener('click', () => {
+    window.location.href = './';
+})
+
+logoCube.addEventListener('mouseover', () => {
+    navbar.style.backgroundColor = 'rgba(224, 183, 57,0.3)'
+    navbar.style.transition = '4s'
+})
+
+logoCube.addEventListener('mouseout', () => {
+    navbar.style.backgroundColor = 'rgba(29, 31, 31, 0.7)'
+    navbar.style.transition = '2s'
+})
+
+logoName.addEventListener('click', () => {
+    window.location.href = './';
+})
+
+logoName.addEventListener('mouseover', () => {
+    navbar.style.backgroundColor = 'rgba(224, 183, 57,0.3)'
+    navbar.style.transition = '4s'
+})
+
+logoName.addEventListener('mouseout', () => {
+    navbar.style.backgroundColor = 'rgba(29, 31, 31, 0.7)'
+    navbar.style.transition = '2s'
+})
+
+const bannerImgChanger = () => {
+    banner.style.opacity = '0'
+    bannerImgIndex = (bannerImgIndex + 1) % bannerImg.length;
+    banner.style.background = `url(${bannerImg[bannerImgIndex]})`;
+    
+    if (bannerImgIndex === 0) {
+        banner.style.backgroundSize = '2100px';
+        banner.style.backgroundPosition = '0 -400px'
+        banner.style.backgroundAttachment = 'fixed';
+        banner.style.opacity = '1'
+        banner.style.transition = 'opacity 1s ease-in, background-size 0s, background-position 0s;';
+        setTimeout(() => {
+            banner.style.opacity = '0'
+        }, 7000)
+
+    } else if (bannerImgIndex === 1) {
+        banner.style.backgroundSize = '2000px';
+        banner.style.backgroundAttachment = 'fixed';
+        banner.style.opacity = '1'
+        banner.style.transition = 'opacity 1s ease-in, background-size 0s, background-position 0s;';
+        setTimeout(() => {
+            banner.style.opacity = '0'
+        }, 7000)
+
+    } else if (bannerImgIndex === 2) {
+        banner.style.backgroundAttachment = 'fixed';
+        banner.style.opacity = '1'
+        banner.style.transition = 'opacity 1s ease-in, background-size 0s, background-position 0s;';
+        setTimeout(() => {
+            banner.style.opacity = '0'
+        }, 7000)
+    } else if (bannerImgIndex === 3) {
+        banner.style.backgroundAttachment = 'fixed';
+        banner.style.opacity = '1'
+        banner.style.transition = 'opacity 1s ease-in, background-size 0s, background-position 0s;';
+        setTimeout(() => {
+            banner.style.opacity = '0'
+        }, 7000)
+    }
+};
+
+bannerImgChanger();
+
+setInterval(bannerImgChanger, 8000)
+
+instagram.addEventListener('click', () => {
+    window.open('https://www.instagram.com/stonesvillage/', '_blank')
+})
---
+++ .history/index_20231026173442.js
@@ -0,0 +1,92 @@
+const banner = document.querySelector('.banner1');
+const bannerImg = ['./resources/bg1.jpg', './resources/bg2.jpg', './resources/bg3.jpg', './resources/bg4.jpg'];
+let bannerImgIndex = 0;
+const logoName = document.querySelector('#logo-name');
+const logoCube = document.querySelector('#logo-cube');
+const navbar = document.querySelector('.navbar');
+const instagram = document.querySelector('#example-instagram');
+const instagramBanner = document.querySelector('.banner-overlay button');
+
+if (instagramBanner) {
+    instagramBanner.addEventListener('click', () => {
+        window.location.href = './inventory';
+    });
+}
+
+
+logoCube.addEventListener('click', () => {
+    window.location.href = './';
+})
+
+logoCube.addEventListener('mouseover', () => {
+    navbar.style.backgroundColor = 'rgba(224, 183, 57,0.3)'
+    navbar.style.transition = '4s'
+})
+
+logoCube.addEventListener('mouseout', () => {
+    navbar.style.backgroundColor = 'rgba(29, 31, 31, 0.7)'
+    navbar.style.transition = '2s'
+})
+
+logoName.addEventListener('click', () => {
+    window.location.href = './';
+})
+
+logoName.addEventListener('mouseover', () => {
+    navbar.style.backgroundColor = 'rgba(224, 183, 57,0.3)'
+    navbar.style.transition = '4s'
+})
+
+logoName.addEventListener('mouseout', () => {
+    navbar.style.backgroundColor = 'rgba(29, 31, 31, 0.7)'
+    navbar.style.transition = '2s'
+})
+
+const bannerImgChanger = () => {
+    banner.style.opacity = '0'
+    bannerImgIndex = (bannerImgIndex + 1) % bannerImg.length;
+    banner.style.background = `url(${bannerImg[bannerImgIndex]})`;
+    
+    if (bannerImgIndex === 0) {
+        banner.classList.add('banner1');
+        banner.style.opacity = '1'
+        
+        setTimeout(() => {
+            banner.style.opacity = '0'
+        }, 7000)
+
+    } else if (bannerImgIndex === 1) {
+        banner.classList.add('banner2');
+        banner.classList.remove('banner1')
+        banner.classList.remove('banner3')
+        banner.classList.remove('banner4')
+        banner.style.opacity = '1'
+        
+        setTimeout(() => {
+            banner.style.opacity = '0'
+        }, 7000)
+
+    } else if (bannerImgIndex === 2) {
+        banner.classList.add('banner3');
+        banner.style.opacity = '1'
+        
+        setTimeout(() => {
+            banner.style.opacity = '0'
+        }, 7000)
+    } else if (bannerImgIndex === 3) {
+        banner.classList.add('banner4');
+        banner.style.opacity = '1'
+        
+        setTimeout(() => {
+            banner.style.opacity = '0'
+        }, 7000)
+    }
+};
+
+bannerImgChanger();
+
+setInterval(bannerImgChanger, 8000)
+
+instagram.addEventListener('click', () => {
+    window.open('https://www.instagram.com/stonesvillage/', '_blank')
+})
